d8d6337b7bc7f43bf9ea892886f8a8d35715e679
Luciano Filho <lvgalvaofilho@gmail.com>
2025-03-18 22:12:23 -0300

    relatorio

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from ydata_profiling import ProfileReport
 
 df = pd.read_csv('data.csv')
 profile = ProfileReport(df, title="Profiling Report")
-profile.to_file("output.html")
+profile.to_file("output.json")
